66f77e5ce14d7d0b3ea1effc7779c264b4aa3c03
Sydney <github@sweetiebelle.org>
2021-09-29 19:55:27 -0400

    Fix a bug. Herp-a-derp.

--- 8119/src/main/java/com/example/examplemod/ExampleMod.java
+++ 8119/src/main/java/com/example/examplemod/ExampleMod.java
@@ -15,36 +15,34 @@ import net.minecraftforge.fml.common.Mod;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
 public class ExampleMod {
-	// Directly reference a log4j logger.
-	private static final Logger LOGGER = LogManager.getLogger();
+     // Directly reference a log4j logger.
+     private static final Logger LOGGER = LogManager.getLogger();
 
-	public ExampleMod() {
+     public ExampleMod() {
+          // Register ourselves for server and other game events we are interested in
+          MinecraftForge.EVENT_BUS.register(this);
+     }
 
-		// Register ourselves for server and other game events we are interested in
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+     @SubscribeEvent
+     public void onBaseSpawnerActivationEvent(BaseSpawnerActivationEvent event) {
+          Level level = event.getLevel();
+          BlockPos block = event.getPos();
+          if (level.isClientSide)
+               return;
+          if (isNearbyPlayerGlowing(level, block.getX() + 0.5D, block.getY() + 0.5D, block.getZ() + 0.5D, 16)) {
+               event.setResult(Result.ALLOW);
+               LOGGER.info("Allowing spawn!");
+          } else {
+               event.setResult(Result.DENY);
+               LOGGER.info("Denying spawn!");
+          }
+     }
 
-	@SubscribeEvent
-	public void onBaseSpawnerActivationEvent(BaseSpawnerActivationEvent event) {
-		Level level = event.getLevel();
-		BlockPos block = event.getPos();
-		if (level.isClientSide)
-			return;
-		if (isNearbyPlayerGlowing(level, block.getX() + 0.5D, block.getY() + 0.5D, block.getZ() + 0.5D, 16)) {
-			event.setResult(Result.ALLOW);
-			LOGGER.info("Allowing spawn!");
-		} else {
-			event.setResult(Result.DENY);
-			LOGGER.info("Denying spawn!");
-		}
-
-	}
-
-	private boolean isNearbyPlayerGlowing(Level level, double x, double y, double z, double range) {
-		for (Player player : level.players()) {
-			return player.isCurrentlyGlowing() && (player.distanceToSqr(x, y, z) < range * range);
-		}
-		return false;
-	}
-
+     private boolean isNearbyPlayerGlowing(Level level, double x, double y, double z, double range) {
+          for (Player player : level.players()) {
+            if(player.isCurrentlyGlowing() && (player.distanceToSqr(x, y, z) < range * range))
+                return true;
+          }
+          return false;
+     }
 }
